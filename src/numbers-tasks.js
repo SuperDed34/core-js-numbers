a930cc94c1740711e69a09e847fc9f5394b30d29
SuperDed34 <maxnozord@gmail.com>
2023-11-24 20:01:08 +0000

    refactor: getCircleCircumference

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -34,8 +34,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCircleCircumference(radius) {
-  const res = 2 * Math.PI * radius;
-  return +res;
+  return 2 * Math.PI * radius;
 }
 
 /**
